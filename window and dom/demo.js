35a6fef84eee6189e01f04aace6e079560634692
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-07-18 17:21:34 +0200

    constant

--- window and dom/demo.js
+++ window and dom/demo.js
@@ -1,13 +1,13 @@
-let inputElement = document.querySelector('input');
-let remainingChars = document.getElementById('remaining-chars');
+const inputElement = document.querySelector('input');
+const remainingChars = document.getElementById('remaining-chars');
 
-let maxAllowedChars = inputElement.maxLength;
+const maxAllowedChars = inputElement.maxLength;
 
 function inputCounter(event) {
-    let enteredText = event.target.value;
-    let enteredTextLength = enteredText.length;
+    const enteredText = event.target.value;
+    const enteredTextLength = enteredText.length;
 
-    let remainingCharacters = maxAllowedChars - enteredTextLength;
+    const remainingCharacters = maxAllowedChars - enteredTextLength;
 
     remainingChars.textContent = remainingCharacters;
     console.log(remainingCharacters)
