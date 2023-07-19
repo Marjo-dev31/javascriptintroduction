2ea866e89a6340762faba61d2c8aae2212454e21
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-07-19 14:56:34 +0200

    daylieexercise

--- window and dom/exercise-2/exercise.js
+++ window and dom/exercise-2/exercise.js
@@ -5,8 +5,9 @@
 //    - Select the second button by using an "id"
 
 const addButton = document.body.children[2].children[4];
+// const addButton = document.querySelector('button');
 
-const removeButton = document.getElementById('second-button')
+const removeButton = document.getElementById('second-button');
 
 
 // 2) Add "click" event listener to both buttons (with two different functions).
@@ -15,17 +16,17 @@ const removeButton = document.getElementById('second-button')
 //    - Output the second button WITHOUT using the variable in which it's stored
 
 // function clickFunction() {
-//     console.dir(addButton)
+//     console.dir(addButton);
 // }
 
-// addButton.addEventListener('click', clickFunction)
+// addButton.addEventListener('click', clickFunction);
 
 
-// function reclickFunction() {
-//     console.dir(document.getElementById('second-button'))
+// function reclickFunction(event) {
+//     console.dir(event.target);
 // }
 
-// removeButton.addEventListener('click', reclickFunction)
+// removeButton.addEventListener('click', reclickFunction);
 
 
 // 3) Now select and store the paragraphs mentioned in the text you see on the page
@@ -34,39 +35,41 @@ const removeButton = document.getElementById('second-button')
 //      mentioned elements
 //    - If you struggle with DOM drilling, use "ids" instead but watch the solution!
 
-console.dir(document)
+// console.dir(document);
 
-document.body.children[2].children[1].classList='paragraphsSelected'
-document.body.children[2].children[3].classList='paragraphsSelected'
+// document.body.children[2].children[1].classList='paragraphsSelected';
+// document.body.children[2].children[3].classList='paragraphsSelected';
 
-let paragraphsSelected = document.getElementsByClassName('paragraphsSelected')
+// let paragraphsSelected = document.getElementsByClassName('paragraphsSelected');
 
-console.dir(paragraphsSelected)
+// console.dir(paragraphsSelected);
+
+const firstParagraph = document.body.children[2].children[1]
+const thirdParagraph = document.body.children[2].children[3]
 
 // 4) Change the functions from (2) such that:
 //    - The first button removes the third paragraph (i.e. the <p> prior to it)
 //    - The second button changes the background color of the first paragraph to blue
 
+// function clickFunction() {
+//    addButton.previousElementSibling.remove()
+// }
+
 function clickFunction() {
-   document.body.children[2].children[3].remove()
+    thirdParagraph.remove();
 }
+addButton.addEventListener('click', clickFunction);
 
-addButton.addEventListener('click', clickFunction)
-
-const firstParagraph = document.body.children[2].children[1]
-console.dir(firstParagraph)
 
 function reclickfunction() {
-    firstParagraph.style.backgroundColor = 'blue'
+    // firstParagraph.style.backgroundColor = 'blue';
+    firstParagraph.classList.add('blue-bg')
 }
 
-removeButton.addEventListener('click', reclickfunction)
+removeButton.addEventListener('click', reclickfunction);
 
 
 // 5) Solve (4) both by changing the "inline styles" as well as by adding CSS classes
 //    Note: You'll have to add those classes to the styles.css file first!
 
 
-paragraphsSelected.classList='classexercise'
-
-console.dir(paragraphsSelected)
