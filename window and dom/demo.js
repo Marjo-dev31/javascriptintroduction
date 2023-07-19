ad367d92c4e0a86901b4f10b21bf5ce85d43d789
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-07-19 17:45:22 +0200

    condition

--- window and dom/demo.js
+++ window and dom/demo.js
@@ -6,11 +6,21 @@ const maxAllowedChars = inputElement.maxLength;
 function inputCounter(event) {
     const enteredText = event.target.value;
     const enteredTextLength = enteredText.length;
-
     const remainingCharacters = maxAllowedChars - enteredTextLength;
 
     remainingChars.textContent = remainingCharacters;
-    console.log(remainingCharacters)
+    console.log(remainingCharacters);
+
+    if (remainingCharacters <= '10') {
+        remainingChars.classList.add('warning');
+        inputElement.classList.add('warning');
+    } 
 }
 
 inputElement.addEventListener('input', inputCounter);
+
+
+
+
+
+
